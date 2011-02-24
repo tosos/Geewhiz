969fb7aeeba3b6140e8b6c9a870dc06c8ac571c9
Nathan Fabian <ndfabian@gmail.com>
2011-02-24 14:22:47 -0700

    Untested version of dispatcher

---
+++ Dispatcher.cs
@@ -0,0 +1,53 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Dispatcher : MonoBehaviour {
+
+    private Dictionary< string, Stack<GameObject> > serialRecv;
+    private Dictionary< string, List<GameObject> > parallelRecv;
+
+    void Awake () {
+        serialRecv = new Dictionary< string, Stack<GameObject> > ();
+        parallelRecv = new Dictionary< string, List<GameObject> > ();
+    }
+
+    public void Dispatch (string message, object parameter) {
+        if (serialRecv.ContainsKey (message)) {
+            serialRecv[message].Peek().SendMessage (message, parameter);
+        }
+
+        if (parallelRecv.ContainsKey (message)) {
+            foreach (GameObject go in parallelRecv[message]) {
+                go.SendMessage (message, parameter);
+            }
+        }
+    }
+
+    public void Register (string message, GameObject obj, bool isParallel = true) {
+        if (isParallel) {
+            if (!parallelRecv.ContainsKey (message)) {
+                parallelRecv.Add (message, new List<GameObject> ());
+            } 
+            parallelRecv[message].Add(obj);
+        } else {
+            if (!serialRecv.ContainsKey (message)) {
+                serialRecv.Add (message, new Stack<GameObject> ());
+            } 
+            serialRecv[message].Push(obj);
+        }
+    }
+
+    public void Unregister (string message, GameObject obj) {
+        if (parallelRecv.ContainsKey (message)) {
+            parallelRecv[message].Remove (obj);
+        }
+        if (serialRecv.ContainsKey (message)) {
+            if (serialRecv[message].Peek () == obj) {
+                serialRecv[message].Pop ();
+            } else {
+                Debug.LogError ("Trying to unregister serial receiver out of order");
+            }
+        }
+    }
+}
