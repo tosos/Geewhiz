3b61a7dc23eb5b4f73f576bbf34353774eb1023d
Nathan Fabian <ndfabian@gmail.com>
2012-01-04 01:02:08 -0700

    Created a reusable PriorityQueue class

---
+++ PriorityQueue.cs
@@ -0,0 +1,110 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PriorityQueue<T> where T : new() {
+    private List<T> queueImpl;
+
+    public Comparison<T> comparator;
+
+    public PriorityQueue () {
+        queueImpl = new List<T>();
+        // blank initial because 0 index shouldn't be used.
+        queueImpl.Add (new T());
+    }
+
+    public int Count {
+        get {
+            return queueImpl.Count - 1;
+        }
+    }
+
+    public bool Empty () {
+        return queueImpl.Count <= 1;
+    }
+
+    public T Top {
+        get {
+            return queueImpl[1];
+        }
+    }
+
+    public void Add (T item) {
+        queueImpl.Add (item);
+        BubbleUp (queueImpl.Count - 1);
+    }
+
+    public bool Remove (T item) {
+        int ind = queueImpl.IndexOf (item);
+        if (ind > 0) {
+            int last = queueImpl.Count - 1;
+            queueImpl[ind] = queueImpl[last];
+            queueImpl.RemoveAt (last);
+            BubbleDown (ind);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public bool Remove (Predicate<T> matchPred) {
+        T item = queueImpl.Find (matchPred);
+        return Remove (item);
+    }
+
+    public T Find (Predicate<T> matchPred) {
+        return queueImpl.Find (matchPred);
+    }
+
+    public void Update (T item) {
+        int ind = queueImpl.IndexOf (item);
+        if (ind > 0) {
+            // One of these two will be a noop
+            BubbleUp (ind);
+            BubbleDown (ind);
+        }
+    }
+
+    private void BubbleUp (int ind) {
+        while (ind > 1) {
+            int parent = ind / 2;
+            if (comparator (queueImpl[ind], queueImpl[parent]) < 0) {
+                T tmp = queueImpl[parent];
+                queueImpl[parent] = queueImpl[ind];
+                queueImpl[ind] = tmp;
+                ind = parent;
+            } else {
+                break;
+            }
+        }
+    }
+
+    private void BubbleDown (int ind) {
+        while (ind < queueImpl.Count) {
+            int left = ind * 2;
+            int right = ind * 2 + 1;
+
+            if (left >= queueImpl.Count) {
+                break;
+            }
+
+            int minInd;
+            if (right >= queueImpl.Count || 
+                comparator (queueImpl[left], queueImpl[right]) < 0)
+            {
+                minInd = left;
+            }  else {
+                minInd = right;
+            }
+
+            if (comparator (queueImpl[minInd], queueImpl[ind]) < 0) {
+                T tmp = queueImpl[minInd];
+                queueImpl[minInd] = queueImpl[ind];
+                queueImpl[ind] = tmp;
+                ind = minInd;
+            } else {
+                break;
+            }
+        }
+    }
+}
--- Scheduler.cs
+++ Scheduler.cs
@@ -4,6 +4,16 @@ using System.Collections.Generic;
 
 [System.Serializable]
 public class TimerMessage {
+    public TimerMessage () {
+        nextTime = 0.0f;
+    }
+    public TimerMessage (TimerMessage m) {
+        message = m.message;
+        secondsBetween = m.secondsBetween;
+        nextTime = m.nextTime;
+        gameObject = m.gameObject;
+    }
+
     public string message;
     public float secondsBetween;
     [HideInInspector]
@@ -15,7 +25,17 @@ public class TimerMessage {
 public class Scheduler : MonoBehaviour {
 
     public TimerMessage[] messages;
-    private List<TimerMessage> priorityQueue;
+    private PriorityQueue<TimerMessage> priorityQueue;
+
+    private static int Comparison (TimerMessage a, TimerMessage b) {
+        if (a.nextTime < b.nextTime) {
+            return -1;
+        } else if (a.nextTime > b.nextTime) {
+            return 1;
+        } else {
+            return 0;
+        }
+    }
 
     static private Scheduler instance = null;
     static public Scheduler GetInstance () {
@@ -30,19 +50,17 @@ public class Scheduler : MonoBehaviour {
         }
         instance = null;
 
-        priorityQueue = new List<TimerMessage> ();
-        // blank initial because 0 index shouldn't be used.
-        priorityQueue.Add (new TimerMessage ()); 
+        priorityQueue = new PriorityQueue<TimerMessage> ();
+        priorityQueue.comparator = Comparison;
 
 	    for (int i = 0; i < messages.Length; i ++) {
             messages[i].gameObject = null;
             messages[i].nextTime = Time.time + messages[i].secondsBetween;
             
             priorityQueue.Add (messages[i]);
-            BubbleUp (priorityQueue.Count - 1);
         }
 
-        if (priorityQueue.Count > 1) {
+        if (!priorityQueue.Empty ()) { 
             // StartCoroutine (Schedule());
         } else {
             enabled = false;
@@ -56,27 +74,15 @@ public class Scheduler : MonoBehaviour {
         msg.secondsBetween = (repeat ? time : 0.0f);
         msg.gameObject = obj;
         priorityQueue.Add (msg);
-        BubbleUp (priorityQueue.Count - 1);
         enabled = true;
     }
 
     public void UpdateSchedule (float time, string message, GameObject obj = null) {
-        int index = 0;
-        for (int i = 1; i < priorityQueue.Count; i ++) {
-            TimerMessage msg = priorityQueue[i];
-            if (msg.message == message && msg.gameObject == obj) {
-                index = i;
-                break;
-            }
-        }
-        if (index > 0) {
-            float lastTime = priorityQueue[index].nextTime;
-            priorityQueue[index].nextTime = Time.time + time;
-            if (priorityQueue[index].nextTime < lastTime) {
-                BubbleUp (index);
-            } else if (priorityQueue[index].nextTime > lastTime) {
-                BubbleDown (index);
-            }
+        TimerMessage msg = 
+            priorityQueue.Find ((a) => a.message == message && a.gameObject == obj);
+        if (msg != null && msg.nextTime > 0) {
+            msg.nextTime = Time.time + time;
+            priorityQueue.Update (msg);
         } else {
             AddSchedule (time, message, false, obj);
         }
@@ -85,45 +91,30 @@ public class Scheduler : MonoBehaviour {
     public void CancelSchedule (string message, GameObject obj = null) {
         bool continueLoop = true;
         while (continueLoop) {
-            continueLoop = false;
-            for (int i = 1; i < priorityQueue.Count; i ++) {
-                if (priorityQueue[i].message == message && 
-                        (obj == null || priorityQueue[i].gameObject == obj)) {
-
-                    int last = priorityQueue.Count - 1;
-                    priorityQueue[i] = priorityQueue[last];
-                    priorityQueue.RemoveAt (last);
-                    BubbleDown (i);
-
-                    continueLoop = true;
-                    break;
-                }
-            }
+            continueLoop = 
+                priorityQueue.Remove ((a) => a.message == message && (obj == null || a.gameObject == obj));
         }
-        if (priorityQueue.Count <= 1) {
+        if (priorityQueue.Empty ()) {
             enabled = false;
         }
     }
 
     void Update () {
-        if (Time.time > priorityQueue[1].nextTime) {
+        if (Time.time > priorityQueue.Top.nextTime) {
             // store these off so we can move the top level or remove it
-            GameObject obj = priorityQueue[1].gameObject;
-            string message = priorityQueue[1].message;
-            float secondsBetween = priorityQueue[1].secondsBetween; 
+            GameObject obj = priorityQueue.Top.gameObject;
+            string message = priorityQueue.Top.message;
+            float secondsBetween = priorityQueue.Top.secondsBetween; 
 
             // move it before we call the message in case the message modifies the queue
-            if (priorityQueue[1].secondsBetween > 0) {
-                priorityQueue[1].nextTime = Time.time + priorityQueue[1].secondsBetween;
-                BubbleDown (1);
+            if (priorityQueue.Top.secondsBetween > 0) {
+                priorityQueue.Top.nextTime = Time.time + priorityQueue.Top.secondsBetween;
+                priorityQueue.Update (priorityQueue.Top);
             } else {
-                int last = priorityQueue.Count - 1;
-                priorityQueue[1] = priorityQueue[last];
-                priorityQueue.RemoveAt (last);
-                BubbleDown (1);
+                priorityQueue.Remove (priorityQueue.Top);
             }
 
-            if (priorityQueue.Count <= 1) {
+            if (priorityQueue.Empty ()) {
                 enabled = false;
             }
 
@@ -134,73 +125,5 @@ public class Scheduler : MonoBehaviour {
             }
 
         }
-    }
-
-/* TODO need to be able to interrupt the WaitForSeconds when Add or Cancel is called.
-    IEnumerator Schedule () {
-        while (true) {
-            yield return new WaitForSeconds(priorityQueue[1].nextTime - Time.time);
-            if (priorityQueue[1].gameObject) {
-                priorityQueue[1].gameObject.SendMessage (priorityQueue[1].message, 
-                                                         priorityQueue[1].secondsBetween);
-            } else {
-                Dispatcher.GetInstance ().Dispatch (priorityQueue[1].message, 
-                                                    priorityQueue[1].secondsBetween);
-            }
-            if (priorityQueue[1].secondsBetween > 0) {
-                priorityQueue[1].nextTime = Time.time + priorityQueue[1].secondsBetween;
-                BubbleDown (1);
-            } else {
-                int last = priorityQueue.Count - 1;
-                priorityQueue[1] = priorityQueue[last];
-                priorityQueue.RemoveAt (last);
-                BubbleDown (1);
-            }
-        }
-    }
-*/
-
-    void BubbleUp (int ind) {
-        while (ind > 1) {
-            int parent = ind / 2;
-            if (priorityQueue[ind].nextTime < priorityQueue[parent].nextTime) {
-                TimerMessage tmp = priorityQueue[parent];
-                priorityQueue[parent] = priorityQueue[ind];
-                priorityQueue[ind] = tmp;
-                ind = parent;
-            } else {
-                break;
-            }
-        }
-    }
-
-    void BubbleDown (int ind) {
-        while (ind < priorityQueue.Count) {
-            int left = ind * 2;
-            int right = ind * 2 + 1;
-
-            if (left >= priorityQueue.Count) {
-                break;
-            }
-
-            int minInd;
-            if (right >= priorityQueue.Count || 
-                priorityQueue[left].nextTime < priorityQueue[right].nextTime) 
-            {
-                minInd = left;
-            }  else {
-                minInd = right;
-            }
-
-            if (priorityQueue[minInd].nextTime < priorityQueue[ind].nextTime) {
-                TimerMessage tmp = priorityQueue[minInd];
-                priorityQueue[minInd] = priorityQueue[ind];
-                priorityQueue[ind] = tmp;
-                ind = minInd;
-            } else {
-                break;
-            }
-        }
-        
     }
 }
