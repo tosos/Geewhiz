0bd97e464a96e63fe32ae1fd56b44be20a3228ae
Nathan Fabian <ndfabian@gmail.com>
2016-08-13 11:13:43 -0600

    Ignore when we're over GUI elements in Canvas

--- MouseDispatch.cs
+++ MouseDispatch.cs
@@ -1,17 +1,25 @@
 using UnityEngine;
+using UnityEngine.EventSystems;
+
 using System.Collections;
 
 public class MouseDispatch : MonoBehaviour {
 
 	// Update is called once per frame
 	void Update () {
-        for (int i = 0; i < 3; i ++) {
-	        if (Input.GetMouseButtonDown (i)) {
-                Dispatcher.instance.Dispatch ("MouseButtonDown", i);
-            }
-	        if (Input.GetMouseButtonUp (i)) {
-                Dispatcher.instance.Dispatch ("MouseButtonUp", i);
-            }
-        }
+		if (EventSystem.current.IsPointerOverGameObject()) {
+			return;
+		}
+
+		if (EventSystem.current.currentSelectedGameObject == null) {
+        	for (int i = 0; i < 3; i ++) {
+	        	if (Input.GetMouseButtonDown (i)) {
+                	Dispatcher.instance.Dispatch ("MouseButtonDown", i);
+            	}
+	        	if (Input.GetMouseButtonUp (i)) {
+                	Dispatcher.instance.Dispatch ("MouseButtonUp", i);
+            	}
+        	}
+		}
 	}
 }
