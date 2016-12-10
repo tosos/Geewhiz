be3ccac40d3089331576c7e30e215b3223290eac
Nathan Fabian <ndfabian@gmail.com>
2016-12-10 09:58:38 -0700

    Modifiers, but not really great.

--- InputDispatch.cs
+++ InputDispatch.cs
@@ -3,11 +3,27 @@ using System.Collections;
 
 public class InputDispatch : MonoBehaviour 
 {
+	public enum Modifier {
+		None = 0x00,
+		Shift = 0x01,
+		Ctrl = 0x02,
+		CtrlShift = 0x03,
+		Alt = 0x04,
+		ShiftAlt = 0x05,
+		CtrlAlt = 0x06,
+		CtrlAltShift = 0x07
+	}
+	[System.Serializable]
+	public class Definition {
+		public string button;
+		public Modifier mod;
+	}
+	public Definition[] definitions;
+
 	public bool CheckAnyKey;
-	public string[] buttons;
 
 	void Start () {
-		if (!CheckAnyKey && buttons.Length == 0) {
+		if (!CheckAnyKey && definitions.Length == 0) {
 			enabled = false;
 		}	
 	}
@@ -17,13 +33,28 @@ public class InputDispatch : MonoBehaviour
         if (CheckAnyKey && Input.anyKeyDown) {
             Dispatcher.instance.Dispatch ("AnyKeyDown", ""); 
         }
+
+		int modVal = 0;
+		if (Input.GetKey (KeyCode.LeftShift) || Input.GetKey (KeyCode.RightShift)) {
+			modVal |= (int)Modifier.Shift;
+		}
+		if (Input.GetKey (KeyCode.LeftControl) || Input.GetKey (KeyCode.RightControl)) {
+			modVal |= (int)Modifier.Ctrl;
+		}
+		if (Input.GetKey (KeyCode.LeftAlt) || Input.GetKey (KeyCode.RightAlt)) {
+			modVal |= (int)Modifier.Alt;
+		}
 	
-		foreach (string button in buttons) {
-			if (Input.GetButtonDown (button)) {
-				Dispatcher.instance.Dispatch ("ButtonDown" + button);
+		foreach (Definition def in definitions) {
+			if ((int)def.mod != modVal) {
+				continue;
 			}
-			if (Input.GetButtonUp (button)) {
-				Dispatcher.instance.Dispatch ("ButtonUp" + button);
+
+			if (Input.GetButtonDown (def.button)) {
+				Dispatcher.instance.Dispatch ("ButtonDown" + def.button);
+			}
+			if (Input.GetButtonUp (def.button)) {
+				Dispatcher.instance.Dispatch ("ButtonUp" + def.button);
 			}
 		}
 	}
